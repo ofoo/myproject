282a314c454e4193b4dd1d9871331fafaf2456ca
唐十三 <746790793@qq.com>
2020-04-23 02:23:21 +0800

    提交代码

--- fengyulou/src/main/resources/sql/fengyulou.sql
+++ fengyulou/src/main/resources/sql/fengyulou.sql
@@ -1,5 +1,5 @@
-﻿# Host: localhost  (Version 5.7.3-m13)
-# Date: 2020-04-21 21:27:53
+﻿# Host: localhost  (Version 5.7.27-log)
+# Date: 2020-04-23 02:23:00
 # Generator: MySQL-Front 6.1  (Build 1.26)
 
 
@@ -21,6 +21,7 @@ CREATE TABLE `fengyulou_member` (
 # Data for table "fengyulou_member"
 #
 
+INSERT INTO `fengyulou_member` VALUES (1,'练束梅',1,'',0),(2,'唐嫣',2,'',0),(3,'唐嫣',2,'',0),(4,'郭碧婷',2,'',0),(5,'高圆圆',2,'123',0),(6,'多对多',1,'123423421',1),(7,'321421',2,'1234',1),(8,'的撒打发222',1,'2222',1);
 
 #
 # Structure for table "fengyulou_member_label"
@@ -38,6 +39,7 @@ CREATE TABLE `fengyulou_member_label` (
 # Data for table "fengyulou_member_label"
 #
 
+INSERT INTO `fengyulou_member_label` VALUES (1,'123',0),(2,'美女',0),(3,'3333222',1),(4,'3333444',1),(5,'3333444555',1);
 
 #
 # Structure for table "fengyulou_project"
@@ -55,6 +57,7 @@ CREATE TABLE `fengyulou_project` (
 # Data for table "fengyulou_project"
 #
 
+INSERT INTO `fengyulou_project` VALUES (1,'123',0),(2,'123123423',1),(3,'44',1),(4,'4442222',1);
 
 #
 # Structure for table "fengyulou_task"
@@ -67,6 +70,8 @@ CREATE TABLE `fengyulou_task` (
   `sketch` varchar(255) NOT NULL DEFAULT '' COMMENT '任务简述',
   `task_label_id` int(11) NOT NULL DEFAULT '0' COMMENT '任务标签id',
   `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '任务状态 0未完成 1已完成',
+  `finish_time` date DEFAULT NULL COMMENT '完成时间',
+  `member_id` int(11) DEFAULT '0' COMMENT '人员id',
   `delete` tinyint(3) NOT NULL DEFAULT '0' COMMENT '0正常 1删除',
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='任务表';
@@ -75,6 +80,7 @@ CREATE TABLE `fengyulou_task` (
 # Data for table "fengyulou_task"
 #
 
+INSERT INTO `fengyulou_task` VALUES (1,1,'123123123',1,1,'2020-04-23',4,0);
 
 #
 # Structure for table "fengyulou_task_label"
@@ -92,6 +98,7 @@ CREATE TABLE `fengyulou_task_label` (
 # Data for table "fengyulou_task_label"
 #
 
+INSERT INTO `fengyulou_task_label` VALUES (1,'123',0),(2,'111222123',1),(3,'111222',1),(4,'111222333',1);
 
 #
 # Structure for table "fengyulou_user"
@@ -102,7 +109,7 @@ CREATE TABLE `fengyulou_user` (
   `Id` int(11) NOT NULL AUTO_INCREMENT,
   `login_name` varchar(255) NOT NULL DEFAULT '' COMMENT '账号',
   `password` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
-  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
+  `name` varchar(255) NOT NULL DEFAULT '' COMMENT '姓名',
   `delete` tinyint(3) NOT NULL DEFAULT '0' COMMENT '0正常 1删除',
   PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
@@ -111,3 +118,4 @@ CREATE TABLE `fengyulou_user` (
 # Data for table "fengyulou_user"
 #
 
+INSERT INTO `fengyulou_user` VALUES (1,'guochao','D19BE809FA02E18DB9A34E905F08AB84','郭超',0);
