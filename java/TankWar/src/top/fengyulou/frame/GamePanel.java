7ee936fcb3921da5e9badcb8fcbec1119e51c105
郭超 <guochao@nxb100.com>
2020-07-01 14:14:00 +0800

    提交代码

--- java/TankWar/src/top/fengyulou/frame/GamePanel.java
+++ java/TankWar/src/top/fengyulou/frame/GamePanel.java
@@ -1,18 +1,19 @@
 package top.fengyulou.frame;
 
-import top.fengyulou.model.Base;
-import top.fengyulou.model.Boom;
-import top.fengyulou.model.Bullet;
-import top.fengyulou.model.Tank;
+import top.fengyulou.model.*;
 import top.fengyulou.model.wall.Wall;
 import top.fengyulou.type.GameType;
+import top.fengyulou.type.TankType;
+import top.fengyulou.util.ImageUtil;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Vector;
 
 /**
  * 游戏面板
@@ -45,6 +46,56 @@ public class GamePanel extends JPanel implements KeyListener {
     private Random r = new Random();//随机数对象
     private int createBotTimer=0;//生产电脑计时器
     private Tank survivor;//（玩家）幸存者，用于绘制最后一个爆炸效果
+
+    public GamePanel(MainFrame frame,int level,GameType gameType){
+        this.frame=frame;
+        this.level=level;
+        setBackground(Color.WHITE);//面板使用白色背景
+        init();//初始化组件
+        Thread t = FreshThread;//创建游戏帧刷新线程
+        t.start();//启动线程
+        addListener();//开启监听
+    }
+
+    /**
+     * 组件初始化
+     */
+    private void init(){
+        bullets=new ArrayList<>();//实例化子弹集合
+        allTanks=new ArrayList<>();//实例化所有坦克集合
+        walls=new ArrayList<>();//实例化所有墙块集合
+        boomImage=new ArrayList<>();//实例化爆炸效果集合
+
+        image = new BufferedImage(749,572,BufferedImage.TYPE_INT_RGB);//实例化主图片，采用面板实际大小
+        g2=image.createGraphics();//获取主图片绘图对象
+
+        playerTanks=new ArrayList<>();//实例化玩家坦克集合
+        play1=new Tank(278,537, ImageUtil.PLAYER1_UP_IMAGE_URL,this, TankType.player1);//实例化玩家1
+        if (gameType==GameType.TWO_PLAYER){//如果是双人模式
+            play2=new Tank(448,537,ImageUtil.PLAYER2_UP_IMAGE_URL,this,TankType.player2);//实例化玩家2
+            playerTanks.add(play2);//玩家坦克集合添加玩家2
+        }
+        playerTanks.add(play1);//玩家坦克集合添加玩家1
+
+        botTanks=new Vector<>();//实例化电脑坦克集合
+        botTanks.add(new Bot(botX[0],1,this,TankType.bot));//在第一个位置添加电脑
+        botTanks.add(new Bot(botX[1],1,this,TankType.bot));//在第二个位置添加电脑
+        botTanks.add(new Bot(botX[2],1,this,TankType.bot));//在第三个位置添加电脑
+        botReadyCount-=3;//准备出场的坦克总数减去初始化数量
+        allTanks.addAll(playerTanks);//所有坦克集合添加玩家坦克集合
+        allTanks.addAll(botTanks);//所有坦克集合添加电脑坦克集合
+        base = new Base(367,532);//实例化基地
+        initWalls();//初始化地图中的墙块
+    }
+
+    /**
+     * 初始化地图中的墙块
+     */
+    public void initWalls(){
+        Map map = Map.getMap(level);//获取当前关卡的地图对象
+        walls.addAll(map.getWalls());//墙块集合添加当前地图中所有墙块
+        walls.add(base);//墙块集合添加基地
+    }
 
     /**
      * 获取所有墙块集合
--- java/TankWar/src/top/fengyulou/frame/LoginPanel.java
+++ java/TankWar/src/top/fengyulou/frame/LoginPanel.java
@@ -6,6 +6,7 @@ import top.fengyulou.util.ImageUtil;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.io.File;
 import java.io.IOException;
@@ -49,6 +50,38 @@ public class LoginPanel extends JPanel implements KeyListener {
         g.drawString("1 PLAYER", 350, 400);//绘制第一行文字
         g.drawString("2 PLAYER", 350, 460);//绘制第二行文字
         g.drawImage(tank, 280, tankY, this);//绘制坦克图标
+    }
+
+    /**
+     * 当按键按下时
+     * @param e
+     */
+    public void keyPressed(KeyEvent e){
+        int code = e.getKeyCode();//获取按下的键盘值
+        switch (code){//判断按键值
+            case KeyEvent.VK_W://如果按下的是“W”，效果同下
+            case KeyEvent.VK_UP://如果按下的是“↑”，效果同下
+            case KeyEvent.VK_S://如果按下的时“S”，效果同下
+            case KeyEvent.VK_DOWN://如果按下的是“↓”
+                if (tankY==y1){//如果坦克图标在第一个位置
+                    tankY=y2;//将图标放在第二个位置
+                }else{
+                    tankY=y1;//将图标放在第一个位置
+                }
+                repaint();//重绘组件
+                break;
+            case KeyEvent.VK_Y://如果按下的时“Y”，效果同下
+            case KeyEvent.VK_NUMPAD1://如果按下的是小键盘1，效果同下
+            case KeyEvent.VK_ENTER://如果按下的是“Enter”，效果同下
+                if (tankY==y1){//如果坦克图标在第一个位置
+                    type = GameType.ONE_PLAYER;//游戏模式为单人模式
+                }else{
+                    type=GameType.TWO_PLAYER;//游戏模式为双人模式
+                }
+                gotoLevelPanel();//跳转关卡面板
+                break;
+
+        }
     }
 
     /**
